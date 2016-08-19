e602f93da633cf90e7ade7a1eca7137fe65353ec
ajaylard <ajay.lard@gmail.com>
2016-08-19 11:32:41 +0100

    Adding php

---
+++ js/app.js
@@ -0,0 +1,50 @@
+console.log("hi");
+
+//BASIC GAME:
+//user presses big-buttons to generate big numbers
+//user presses small-buttons to generate small numbers
+//user presses go-button to generate answer at top of screen
+//timer starts for 30 secs
+//after 30secs is up dim clock image and display "times up".
+//user presses solution button to give up 
+//stop clock when solution displays on the left side of page.
+//user presses reset to clear everything on page
+
+//-------------------------------------------------------
+
+//PSEUDO CODE:
+//set out all variables for each all elements like the calculator
+
+
+//user presses big-buttons to generate big numbers
+//create buttons with click event listeners to make the buttons display html in div's
+//get numbers from an array with big numbers [100, 75, 50, 25]
+
+//user presses small-buttons to generate small numbers
+//get numbers from an array with number (1,2,3,4,5,6,7,8,9,10) 
+
+//these number will go into new array called top-numbers (max length of array is 6).
+
+//user presses go-button to generate answer (which will be displayed in answer-div).
+//cannot press go-button without all the six top-numbers being filled.
+//calculation of answer using random indexes (the 6 numbers) from top-numbers array.
+//
+
+
+//timer starts for 30 secs when go-button is pressed
+//to make image work use 30 images of clock in different positions and run in sequence
+//or use video ---- https://www.youtube.com/watch?v=M2dhD9zR6hk
+//
+//after 30secs is up dim clock image and display "times up".
+
+
+
+//user presses solution button to give up
+//stop clock video when solution displays on the left side of page.
+
+//display solution on the left side of page
+//this will be converting calulation into strings of text.
+//use index values used by the computer to solve and return these in each step of calulation.
+
+//user presses reset to clear everything on page
+//
