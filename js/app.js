7aa0586b4b3b084c4fd0926f1642ae02455164a4
ajaylard <ajay.lard@gmail.com>
2016-08-20 15:14:18 +0100

    numbers now adding to boxes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-console.log("hi");
-
-//BASIC GAME:
+// window.onload = function(){ 
+//BASIC NUMBER GAME:
 //user presses big-buttons to generate big numbers
 //user presses small-buttons to generate small numbers
 //user presses go-button to generate answer at top of screen
@@ -12,18 +11,19 @@ console.log("hi");
 
 //-------------------------------------------------------
 
-//PSEUDO CODE:
+//FURTHER PSEUDO CODE:
 //set out all variables for each all elements like the calculator
 
+//Choice of numbers "1,2,3,4,5,6,7,8,9,10,25,50,75,100" picked from array [0,1,2,3,4,5,6,7,8,9,10,11,12,13].
 
-//user presses big-buttons to generate big numbers
+//user presses big-buttons to generate numbers from array places [10,11,12,13] 
 //create buttons with click event listeners to make the buttons display html in div's
-//get numbers from an array with big numbers [100, 75, 50, 25]
+//do an eventlistener to pick [i] from array and display this in div element.
 
 //user presses small-buttons to generate small numbers
-//get numbers from an array with number (1,2,3,4,5,6,7,8,9,10) 
+//get numbers from array places [0-9]
 
-//these number will go into new array called top-numbers (max length of array is 6).
+//these numbers will go into new array called top-numbers (max length of array is 6). function topNumbers ()
 
 //user presses go-button to generate answer (which will be displayed in answer-div).
 //cannot press go-button without all the six top-numbers being filled.
@@ -33,7 +33,7 @@ console.log("hi");
 
 //timer starts for 30 secs when go-button is pressed
 //to make image work use 30 images of clock in different positions and run in sequence
-//or use video ---- https://www.youtube.com/watch?v=M2dhD9zR6hk
+//or use video??? ---- https://www.youtube.com/watch?v=M2dhD9zR6hk
 //
 //after 30secs is up dim clock image and display "times up".
 
@@ -48,3 +48,91 @@ console.log("hi");
 
 //user presses reset to clear everything on page
 //
+
+///////////////////////////////////////////////////////////////////////
+
+  // var bigNum = ["25", "50", "75", "100"];
+//   var bigUsed =;
+//   var smallUsed = new Array();
+//   var TargetNum=0 ;
+//   // var MaxNum=5 ;
+//   // var CurrCecil=0 ;
+//   // var MaxCecil=2 ;
+//   // var NumOffSet=3 ;
+//   // var CardOffSet=9 ;
+//   // var ManEntry=0 ;
+//   // var Value=0 ;
+
+
+// var smallButton = document.getElementById("smallbutton");
+// var targetBox = document.getElementById("targetbox");
+
+// var selectedNumbers = [];
+// var bigNumbers = [25, 50, 75, 100];
+
+// bigbutton.onclick = function() {
+//   var number = bigNumbers[Math.floor(Math.random()*bigNumbers.length)];
+//   selectedNumbers.push(number);
+
+//   console.log(selectedNumbers);
+// }
+
+// smallButton.onclick = function (){
+//   var number = (Math.floor((Math.random() * 10) + 1));
+//   selectedNumbers.push(number);
+
+//   console.log(selectedNumbers);
+// }
+
+
+// };
+
+// function fillTargetBox (targetBox, ){
+//   targetBox.innerHTML  = player;
+//   targetBox.classList += player;
+// }
+
+
+// $(function getSmallNum(){
+//     // var smallNum = (document.getElementById("smallbutton").
+//     $('#smallbutton').click(function(){
+//       console.log ("clicked");
+//     });
+// })
+
+
+$(start)
+
+function start() {
+  var $displays       = $('td');
+  var $bigButton      = $('#bigbutton');
+  var $smallButton    = $('#smallbutton');
+
+  var bigNumbers      = [25, 50, 75, 100];
+  var smallNumbers    = [1,2,3,4,5,6,7,8,9,10];
+  var selectedNumbers = []; 
+
+  if (selectedNumbers.length < 6) {
+
+    $bigButton.on('click', function() {
+      var number = bigNumbers[Math.floor(Math.random()*bigNumbers.length)];
+      selectedNumbers.push(number);
+      $($displays[selectedNumbers.length -1]).html(number)
+    });
+
+    $smallButton.on('click', function() {
+      var number = smallNumbers[Math.floor(Math.random()*smallNumbers.length)];
+      selectedNumbers.push(number);
+      $($displays[selectedNumbers.length -1]).html(number)
+    });
+
+  }
+
+}
+
+
+
+
+
+
+
